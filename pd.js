73e657a38d36f63cc793ceb770e21cc0734da0e7
vikash <champvikash4@gmail.com>
2022-05-04 15:41:34 +0530

    done

---
+++ pd.js
@@ -0,0 +1,5 @@
+// console.log("hdsdjjk")
+// document.querySelector('.cross').style.display='none';
+// document.querySelector('.hamburg').addEventListener("click",()=>{
+//     document.querySelector('.sidebar').classList.toggle('sidebarGO');
+// })
